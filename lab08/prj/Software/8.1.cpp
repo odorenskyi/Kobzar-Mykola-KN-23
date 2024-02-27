d06abc5386669d97fbf3bdda14dc672c15b9716c
Kobzar <kobzarkoly1109@gmail.com>
2024-02-28 01:39:13 +0200

    Update 8.1.cpp
    витер коментарі в коді

--- lab08/prj/Software/8.1.cpp
+++ lab08/prj/Software/8.1.cpp
@@ -1,29 +1,29 @@
-#include <cmath> // Підключаємо бібліотеку cmath для використання математичних функцій
-#include <iostream> // Підключаємо бібліотеку iostream для вводу/виводу
-#include <windows.h> // Підключає мову
+#include <cmath> 
+#include <iostream> 
+#include <windows.h> 
 
-// Функція для обчислення виразу
+
 double calculateS(double x, double y, double z) {
-    // Обчислюємо вираз за формулою
+    
     double S = (sin(x) / sqrt((y*z) / (x+y))) + 3 * pow(y, 5);
-    return S; // Повертаємо результат
+    return S; 
 }
 
 int main() {
     SetConsoleCP(1251);
-    SetConsoleOutputCP(1251); //встановили шифрування
-    double x, y, z; // Оголошуємо змінні x, y, z
+    SetConsoleOutputCP(1251); 
+    double x, y, z; 
 
-    // Запитуємо у користувача ввести значення x, y, z
-    std::cout << "Введіть значення x, y, z: ";
+    
+    std::cout << "Г‚ГўГҐГ¤ВіГІГј Г§Г­Г Г·ГҐГ­Г­Гї x, y, z: ";
     std::cin >> x >> y >> z;
 
-    // Обчислюємо S за допомогою функції calculateS
+    
     double S = calculateS(x, y, z);
 
-    // Виводимо результат
-    std::cout << "Результат: " << S << std::endl;
+    
+    std::cout << "ГђГҐГ§ГіГ«ГјГІГ ГІ: " << S << std::endl;
 
-    return 0; // Завершуємо програму
+    return 0; 
 }
 
